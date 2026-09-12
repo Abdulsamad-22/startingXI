48e441d8cab2825f1eadbf77b9a94fe69e481912
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-09-12 23:54:21 +0100

    feat: open depending what is selected in control bar

---
+++ components/SidePanel.tsx
@@ -0,0 +1,27 @@
+import { TeamDetailsForm } from "./TeamDetails";
+import { FormationPanel } from "./FormationPanel";
+import type { Tab } from "./ControlBar";
+
+export function SidePanel({
+  activeTab,
+  allFormations,
+}: {
+  activeTab: Tab;
+  allFormations: {
+    id: string;
+    name: string;
+    slots: any;
+    format_size: number;
+  }[];
+}) {
+  if (activeTab !== "Team Details" && activeTab !== "Formation") return null;
+
+  return (
+    <div className="w-full md:w-72 bg-[#1D2A25] rounded-xl p-4 shrink-0">
+      {activeTab === "Team Details" && <TeamDetailsForm />}
+      {activeTab === "Formation" && (
+        <FormationPanel allFormations={allFormations} />
+      )}
+    </div>
+  );
+}

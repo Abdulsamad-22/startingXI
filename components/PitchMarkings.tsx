826aabaf003073b6cad95db865077930defe6c0d
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-09-18 21:23:12 +0100

    fix: add style attribute to show markings in exported video

--- components/PitchMarkings.tsx
+++ components/PitchMarkings.tsx
@@ -6,7 +6,13 @@ export function PitchMarkings({
   return (
     <svg
       viewBox="0 0 100 150"
-      className="absolute inset-0 w-full h-full"
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+      }}
       preserveAspectRatio="none"
     >
       <rect

cc4fce324250e8e7cbcf9c948eae62847c25e1a1
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-09-23 21:32:26 +0100

    update: add ngrok to test payment and run secured http

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: [
-    '@remotion/bundler',
-    '@remotion/renderer',
-    '@remotion/cli',
+    "@remotion/bundler",
+    "@remotion/renderer",
+    "@remotion/cli",
   ],
-}
+
+  allowedDevOrigins: ["gumdrop-viability-broadband.ngrok-free.dev"],
+};
 
 export default nextConfig;
